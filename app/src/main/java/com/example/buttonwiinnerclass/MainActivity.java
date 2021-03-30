97722a7a90af825c50ad136987a5a813bccefafc
JEONSUAH <junsu3448@gmail.com>
2021-03-30 10:51:08 +0900

    inner class without this

--- app/src/main/java/com/example/buttonwiinnerclass/MainActivity.java
+++ app/src/main/java/com/example/buttonwiinnerclass/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.buttonwiinnerclass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,6 +20,12 @@ public class MainActivity extends AppCompatActivity {
         mTextview1 = findViewById(R.id.textView1);
 
 
-        mButton1.setOnClickListener(new MyOnClickListener(this));
+        mButton1.setOnClickListener(new MyOnClickListener());
+    }
+    private class MyOnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+        mTextview1.setText("You clicked suah's button");
+        }
     }
 }
